981c05a156495118ba25d51b5ebde930a4a2422d
elijacode <pakalnis.elijas@gmail.com>
2019-07-29 18:56:51 +0100

    change TaskManager to be static

--- core/src/main/java/com/github/elijacode/TaskManager.java
+++ core/src/main/java/com/github/elijacode/TaskManager.java
@@ -1,7 +1,21 @@
 package com.github.elijacode;
 
-public interface TaskManager {
+public class TaskManager {
 
-  Task create();
-  void changeDetails(Task task);
+  // == public static methods ==
+  public static Task create() {
+    MessageGenerator.promptTitle();
+    String title = Input.getString();
+    MessageGenerator.promptDescription();
+    String description = Input.getString();
+
+    return new TaskImpl(title, description);
+  }
+
+  public static void changeDetails(Task task) {
+    MessageGenerator.promptTitle();
+    task.setTitle(Input.getString());
+    MessageGenerator.promptDescription();
+    task.setDescription(Input.getString());
+  }
 }
